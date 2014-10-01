0bec630234334a9fbcf7532eecbc089f484c93b8
Alecs Popa <alecs.popa@gmail.com>
2014-10-01 21:58:52 +0300

    Initial Commit

---
+++ Can I do this?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Can I do this?
+//
+//  Created by Alecs Popa on 01/10/14.
+//  Copyright (c) 2014 Alecs Popa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
